aa4dab150962fab10a4239bbfb34e0a4351b6b1d
Johnny Arguedas <johnnyarguedas@hotmail.com>
2017-12-12 22:25:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function logShout (string) {
   console.log(string.toUpperCase( ));
 }
 
-function logWhisper (aWord) {
-  console.log(aWord.toLowerCase( ));
+function logWhisper (string) {
+  console.log(String.toLowerCase( ));
 }
