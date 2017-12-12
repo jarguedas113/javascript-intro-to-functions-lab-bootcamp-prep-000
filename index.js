a2cf58d5b1ebc028596ffeaa1764d880d032017f
Johnny Arguedas <johnnyarguedas@hotmail.com>
2017-12-12 22:26:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,8 @@ function logShout (string) {
 function logWhisper (string) {
   console.log(String.toLowerCase( ));
 }
+
+function sayHiToGrandma (string) {
+  var cantUnswer = "I can't hear you!";
+  if (string.toLowerCase(aWord) === aWord) return cantUnswer;
+}
